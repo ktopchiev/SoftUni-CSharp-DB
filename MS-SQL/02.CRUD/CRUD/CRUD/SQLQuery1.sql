d66bdbf52f05a7785a811a8d5451c6c8776c17d4
ktopchiev <karlostopchiev@gmail.com>
2021-09-24 21:11:31 +0300

    select queries

---
+++ MS-SQL/02.CRUD/CRUD/CRUD/SQLQuery1.sql
@@ -0,0 +1,39 @@
+USE SoftUni
+
+SELECT *
+FROM Departments
+
+SELECT [Name] 
+FROM Departments
+
+SELECT *
+FROM [Employees]
+
+SELECT [FirstName], [LastName], [Salary]
+FROM [Employees]
+
+SELECT CONCAT([FirstName],' ', [MiddleName], '. ', [LastName]) AS FullName
+FROM [Employees]
+
+SELECT [FirstName], [MiddleName], [LastName]
+FROM [Employees]
+
+SELECT CONCAT([FirstName], '.', [LastName], '@softuni.bg') AS [Full Email Address]
+FROM [Employees]
+
+--Find unique salaries
+SELECT DISTINCT [Salary]
+FROM [Employees]
+
+--Find all employees with jobtitle sales representative
+SELECT *
+FROM [Employees]
+WHERE [JobTitle] = 'Sales Representative'
+--
+SELECT [FirstName], [LastName], [JobTitle]
+FROM [Employees]
+WHERE [Salary] BETWEEN 20000 AND 30000
+
+SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName]) AS [Full Name]
+FROM [Employees]
+WHERE [Salary] = 25000 OR [Salary] = 14000 OR [Salary] = 12500 OR [Salary] = 23600
