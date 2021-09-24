5ce8286e49649d288949a21edd7e008fea72a239
ktopchiev <karlostopchiev@gmail.com>
2021-09-24 22:47:28 +0300

    solved to 21 problem

--- MS-SQL/02.CRUD/CRUD/CRUD/SQLQuery1.sql
+++ MS-SQL/02.CRUD/CRUD/CRUD/SQLQuery1.sql
@@ -1,30 +1,30 @@
 USE SoftUni
-
+--
 SELECT *
 FROM Departments
-
+--
 SELECT [Name] 
 FROM Departments
-
+--
 SELECT *
 FROM [Employees]
-
+--
 SELECT [FirstName], [LastName], [Salary]
 FROM [Employees]
-
+--
 SELECT CONCAT([FirstName],' ', [MiddleName], '. ', [LastName]) AS FullName
 FROM [Employees]
-
+--
 SELECT [FirstName], [MiddleName], [LastName]
 FROM [Employees]
-
+--
 SELECT CONCAT([FirstName], '.', [LastName], '@softuni.bg') AS [Full Email Address]
 FROM [Employees]
-
+--
 --Find unique salaries
 SELECT DISTINCT [Salary]
 FROM [Employees]
-
+--
 --Find all employees with jobtitle sales representative
 SELECT *
 FROM [Employees]
@@ -33,17 +33,68 @@ WHERE [JobTitle] = 'Sales Representative'
 SELECT [FirstName], [LastName], [JobTitle]
 FROM [Employees]
 WHERE [Salary] BETWEEN 20000 AND 30000
-
+--
 SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName]) AS [Full Name]
 FROM [Employees]
 WHERE [Salary] = 25000 OR [Salary] = 14000 OR [Salary] = 12500 OR [Salary] = 23600
-
+--
 SELECT [FirstName], [LastName]
 FROM [Employees]
 WHERE [ManagerID] IS NULL
-
+--
 SELECT [FirstName], [LastName], [Salary]
 FROM [Employees]
 WHERE [Salary] > 50000
 ORDER BY [Salary] DESC
+--
+SELECT TOP 5 [FirstName], [LastName]
+FROM [Employees]
+ORDER BY [Salary] DESC
+--
+SELECT [FirstName], [LastName]
+FROM [Employees]
+WHERE [DepartmentId] != 4
+--
+SELECT *
+FROM [Employees]
+ORDER BY [Salary] DESC,
+	[FirstName],
+	[LastName] DESC,
+	[MiddleName]
+--
+CREATE VIEW v_EmployeesSalaries AS
+SELECT [FirstName], [LastName], [Salary]
+FROM [Employees]
+--
+--Replace middle name with empty string if it's null
+CREATE VIEW v_EmployeeNameJobTitle AS
+SELECT CONCAT([FirstName],
+	' ',
+	CASE WHEN MiddleName IS NULL THEN ''
+	ELSE MiddleName END,
+	' ',
+	[LastName]) AS [Full Name],
+	[JobTitle]
+FROM [Employees]
+--
+SELECT DISTINCT [JobTitle]
+FROM [Employees]
+--
 
+SELECT TOP 10 *
+FROM [Projects]
+WHERE [EndDate] IS NOT NULL
+ORDER BY [StartDate],
+	[Name]
+--
+-- Last 7 hired employees
+SELECT TOP 7 [FirstName], [LastName], [HireDate]
+FROM [Employees]
+ORDER BY [HireDate] DESC
+
+UPDATE [Employees]
+SET [Salary] = [Salary] * 1.12
+WHERE [DepartmentId] IN (1, 2, 4, 11)
+SELECT [Salary]
+FROM [Employees]
+--1,2,4,11
