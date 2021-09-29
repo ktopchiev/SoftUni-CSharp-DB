1191f4f1e5129b23176a4f3938fea0c2e984da3e
ktopchiev <karlostopchiev@gmail.com>
2021-09-30 00:05:35 +0300

    Peaks in Rila sort

---
+++ MS-SQL/03.Table relations/Table-relations/Peaks-in-Rila.sql
@@ -0,0 +1,5 @@
+SELECT Mountains.MountainRange, Peaks.PeakName, Peaks.Elevation
+FROM Peaks
+JOIN Mountains ON Peaks.MountainId = Mountains.Id
+WHERE Mountains.MountainRange = 'Rila'
+ORDER BY Peaks.Elevation DESC
