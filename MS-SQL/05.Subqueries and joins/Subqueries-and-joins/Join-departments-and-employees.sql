0255ff7b5276223dbef518cbf7caaffac3fe3077
ktopchiev <karlostopchiev@gmail.com>
2021-10-04 00:39:38 +0300

    join employees and departments

---
+++ MS-SQL/05.Subqueries and joins/Subqueries-and-joins/Join-departments-and-employees.sql
@@ -0,0 +1,8 @@
+SELECT TOP 5 e.EmployeeID
+	,e.FirstName
+	,e.Salary
+	,d.Name AS DepartmentName
+FROM Employees AS e
+INNER JOIN Departments d ON d.DepartmentID = e.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY d.DepartmentID ASC
