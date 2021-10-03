0b0bc8ca8727524a62923de43b9fe092d6e6c4d2
ktopchiev <karlostopchiev@gmail.com>
2021-10-04 00:31:11 +0300

    join employees and departments

---
+++ MS-SQL/05.Subqueries and joins/Subqueries-and-joins/Join-employees-and-departments.sql
@@ -0,0 +1,8 @@
+SELECT e.EmployeeID 
+	,e.FirstName
+	,e.LastName
+	,d.Name
+FROM Employees AS e
+INNER JOIN Departments d ON d.DepartmentID = e.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID ASC
