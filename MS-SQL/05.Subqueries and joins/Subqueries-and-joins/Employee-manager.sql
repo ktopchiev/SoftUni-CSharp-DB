f79263d55d3c9b9d8e5b48bad73e688fbcf2c3df
ktopchiev <karlostopchiev@gmail.com>
2021-10-04 15:16:19 +0300

    self refference query

---
+++ MS-SQL/05.Subqueries and joins/Subqueries-and-joins/Employee-manager.sql
@@ -0,0 +1,9 @@
+SELECT 
+	e.EmployeeID
+	,e.FirstName
+	,e.ManagerID
+	,m.FirstName AS ManagerName
+FROM Employees AS e
+JOIN Employees m ON e.ManagerID = m.EmployeeID
+WHERE e.ManagerID = 3 OR e.ManagerID = 7
+ORDER BY e.EmployeeID ASC
