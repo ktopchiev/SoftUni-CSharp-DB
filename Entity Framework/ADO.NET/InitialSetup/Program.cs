70cf4276dd6fd25084ecfb13f43323990e0b5fc1
ktopchiev <karlostopchiev@gmail.com>
2021-10-21 21:34:01 +0300

    initial setup with one table only

---
+++ Entity Framework/ADO.NET/InitialSetup/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace InitialSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var conn = SetConnection();
+            conn.Open();
+
+            using (conn)
+            {
+                CreateDb(conn);
+                CreateTable(conn);
+            }
+        }
+
+        public static SqlConnection SetConnection()
+        {
+            string connStr = "Server=KTOPCHIEV-LAPTO\\SQLEXPRESS; Database=master; Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connStr);
+            return conn;
+        }
+
+        public static void CreateDb(SqlConnection conn)
+        {
+            string cmdText = "CREATE DATABASE MinionsDB";
+            SqlCommand sqlCmd = new SqlCommand(cmdText, conn);
+
+            var execution = sqlCmd.ExecuteNonQuery();
+        }
+
+        public static void CreateTable(SqlConnection conn)
+        {
+            string createTablesCmdText = "USE MinionsDB" +
+                    "\n" +
+                    "CREATE TABLE Minions" +
+                    "\n(" +
+                    "   \nId INT PRIMARY KEY IDENTITY NOT NULL," +
+                    "   \nName VARCHAR(50) NOT NULL," +
+                    "   \nAge VARCHAR(50) NOT NULL," +
+                    "   \nCountryCode INT FOREIGN KEY REFERENCES Countries (Id)," +
+                    "\n)";
+            SqlCommand cmd = new SqlCommand(createTablesCmdText, conn);
+            int execute = cmd.ExecuteNonQuery();
+            Console.WriteLine(execute);
+        }
+    }
+}
