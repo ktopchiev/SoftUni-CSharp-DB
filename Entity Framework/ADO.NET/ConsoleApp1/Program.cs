9125e91d1e95a397390eab890eb3be76d53bffe2
ktopchiev <karlostopchiev@gmail.com>
2021-10-21 20:03:57 +0300

    Entity framework course initial commit

---
+++ Entity Framework/ADO.NET/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Server=KTOPCHIEV-LAPTO\\SQLEXPRESS;DataBase=SoftUni;Integrated Security=true";
+            SqlConnection connection = new SqlConnection(connString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand("SELECT TOP 5 * FROM Employees", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string firstName = (string)reader["FirstName"];
+                        Console.WriteLine(firstName);
+                    }
+                }
+            }
+
+        }
+    }
+}
