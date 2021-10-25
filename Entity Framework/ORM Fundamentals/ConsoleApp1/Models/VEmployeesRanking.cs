caa670e99a2c4e1655a593503bca42fb4b94abc8
ktopchiev <karlostopchiev@gmail.com>
2021-10-25 18:22:57 +0300

    database first orm model

---
+++ Entity Framework/ORM Fundamentals/ConsoleApp1/Models/VEmployeesRanking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ConsoleApp1.Models
+{
+    public partial class VEmployeesRanking
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public long? Rank { get; set; }
+    }
+}
---
+++ Entity Framework/ORM Fundamentals/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var db = new SoftUniContext();
+            var employees = db.Employees.Where(e => e.FirstName == "Guy")
+                .GroupBy(x => x.Address.AddressText)
+                .Select(x => new { Name = x.Key})
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine($"{e.Name}");
+            }
+
+            //db.Towns.Add(new Town { Name = "Dimitrovgrad" });
+            //db.SaveChanges();
+        }
+    }
+}
