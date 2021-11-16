1f18478b74b79e0b8e3c9b38167253bd37f49707
ktopchiev <karlostopchiev@gmail.com>
2021-11-16 20:17:44 +0200

    Solved get golden books with less than 5k copies

--- Entity Framework/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework/Advanced Querying/BookShop/StartUp.cs
@@ -11,10 +11,11 @@
     {
         public static void Main()
         {
-            using var db = new BookShopContext();
+            using var dbContext = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
-            var command = Console.ReadLine();
-            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            //var command = Console.ReadLine();
+            //Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            Console.WriteLine(GetGoldenBooks(dbContext));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -22,6 +23,27 @@
             var books = context.Books.Where(b => Enum.Parse<AgeRestriction>(command, true) == b.AgeRestriction)
                 .Select(b => new { b.Title })
                 .OrderBy(b => b.Title)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            EditionType editionType = Enum.Parse<EditionType>("gold", true);
+
+            var books = context.Books
+                .Where(b => b.EditionType == editionType)
+                .Where(b => b.Copies < 5000)
+                .Select(b => new { b.Title, b.BookId })
+                .OrderBy(b => b.BookId)
                 .ToList();
 
             StringBuilder sb = new StringBuilder();
