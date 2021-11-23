a8c90a0e546201d2082d40f013f85b377a4a7b2f
ktopchiev <karlostopchiev@gmail.com>
2021-11-23 23:01:13 +0200

    Import Users problem solved

---
+++ Entity Framework/JSON-Processing/ProductShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+            var workingDirectory = Environment.CurrentDirectory;
+            var path = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            var file = path + "\\Datasets\\users.json";
+
+            var inputJson = File.ReadAllText(file);
+
+
+            Console.WriteLine(ImportUsers(context, inputJson));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            var tableUsers = context.Users;
+
+            foreach (var user in users)
+            {
+                tableUsers.Add(user);
+            }
+
+            context.SaveChanges();
+            return $"Successfully imported { context.Users.Count()}";
+        }
+    }
+}
