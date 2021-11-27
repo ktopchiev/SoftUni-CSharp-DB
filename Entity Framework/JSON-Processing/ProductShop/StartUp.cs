47b54c30a3cbff34b99301e97d9416225a04dc7d
ktopchiev <karlostopchiev@gmail.com>
2021-11-27 11:36:47 +0200

    get products in range

--- Entity Framework/JSON-Processing/ProductShop/StartUp.cs
+++ Entity Framework/JSON-Processing/ProductShop/StartUp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using ProductShop.Data;
 using ProductShop.Models;
@@ -19,17 +20,18 @@ namespace ProductShop
             //var file = path + "\\Datasets\\users.json";
             //var datasetFile = path + "\\Datasets\\products.json";
             //var datasetFile = path + "\\Datasets\\categories.json";
-            var datasetFile = path + "\\Datasets\\categories-products.json";
+            //var datasetFile = path + "\\Datasets\\categories-products.json";
 
-            var inputJson = File.ReadAllText(datasetFile);
+            //var inputJson = File.ReadAllText(datasetFile);
 
             //Console.WriteLine(ImportUsers(context, inputJson));
             //Console.WriteLine(ImportProducts(context, inputJson));
             //Console.WriteLine(ImportCategories(context, inputJson));
-            Console.WriteLine(ImportCategoryProducts(context, inputJson));
+            //Console.WriteLine(ImportCategoryProducts(context, inputJson));
+            Console.WriteLine(GetProductsInRange(context));
         }
 
-        //Problem 02
+        ////Problem 01
         public static string ImportUsers(ProductShopContext context, string inputJson)
         {
 
@@ -45,7 +47,7 @@ namespace ProductShop
             return $"Successfully imported { context.Users.Count()}";
         }
 
-        //Problem 03
+        //Problem 02
         public static string ImportProducts(ProductShopContext context, string inputJson)
         {
             List<Product> productObjects = JsonConvert.DeserializeObject<List<Product>>(inputJson);
@@ -57,10 +59,10 @@ namespace ProductShop
 
             context.SaveChanges();
 
-            return $"Successfuly imported {context.Products.Count()}";
+            return $"Successfully imported {context.Products.Count()}";
         }
 
-        //Problem 04
+        //Problem 03
         public static string ImportCategories(ProductShopContext context, string inputJson)
         {
             var dbContext = context.Categories;
@@ -80,7 +82,7 @@ namespace ProductShop
             return $"Successfully imported {dbContext.Count()}";
         }
 
-        //Problem 05
+        ////Problem 04
         public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
         {
             var table = context.CategoryProducts;
@@ -97,5 +99,27 @@ namespace ProductShop
 
             return $"Successfully imported {table.Count()}";
         }
+
+        //Problem 05
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Include(p => p.Seller)
+                .Where(p => p.Price > 500 && p.Price <= 1000)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Price,
+                    p.Seller
+                })
+                .OrderBy(p => p.Price)
+                .ToList();
+
+            var productsJson = JsonConvert.SerializeObject(products, Formatting.Indented);
+
+            return productsJson;
+        }
+
+        //Problem 06
     }
 }
